0a417f38eef78043d197279623dcfe26c3e796ea
loveKino <842914439@qq.com>
2017-03-20 14:55:19 +0800

    opt

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ## xmind file format
 
 https://github.com/xmindltd/xmind/wiki/XMindFileFormat
+
+### count
+
+ruby -Ilib ./bin/count ${dir}
--- test/test_index.rb
+++ test/test_index.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 
+# TODO
+
 class IndexTest < Minitest::Unit::TestCase
     def test_base
         assert_equal true, true
